dd7429d679839a291a84db8fee7bc767ad469912
Riccardo Bini <odracci@gmail.com>
2010-10-17 21:12:10 +0200

    Updated PHPDoc

--- lib/filter/doctrine/PluginsfAssetFolderFormFilter.class.php
+++ lib/filter/doctrine/PluginsfAssetFolderFormFilter.class.php
@@ -3,9 +3,10 @@
 /**
  * PluginsfAssetFolder form.
  *
- * @package    ##PROJECT_NAME##
+ * @package    sfDoctrineAssetsLibraryPlugin
  * @subpackage filter
- * @author     ##AUTHOR_NAME##
+ * @author     Massimiliano Arione
+ * @author     Riccardo Bini
  * @version    SVN: $Id: sfDoctrineFormFilterPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 abstract class PluginsfAssetFolderFormFilter extends BasesfAssetFolderFormFilter
